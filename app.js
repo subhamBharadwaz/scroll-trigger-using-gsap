6e68217f9dfa0b8678f3b3f24b0859b4e1a0570d
unknown <subhamsbharadwaz@gmail.com>
2020-12-08 21:07:10 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.utils.toArray('.section').forEach(section => {
+    ScrollTrigger.create({
+        trigger : section,
+        start : 'top top',
+        pin: true,
+        pinSpacing : false
+    });
+});
+
+
